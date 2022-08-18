aaef09bc1563ff4e2941fef94e91593968892b57
Summer prit singh <70427674+summer1210@users.noreply.github.com>
2022-08-18 19:07:24 +0530

    Time: 362 ms (5.02%), Space: 18.1 MB (31.91%) - LeetHub

---
+++ 135-candy/135-candy.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int candy(vector<int>& rat) {
+     
+        if(rat.size()==0)
+            return 0;
+        if(rat.size()==1)
+            return 1;
+        
+        vector<int> forward(rat.size()),backward(rat.size());
+        forward[0]=1;
+        for(int i=1;i<rat.size();i++)
+        {   
+         if(rat[i]>rat[i-1]){
+             forward[i]=forward[i-1]+1;
+         }else{
+             forward[i]=1;
+         }  
+        }
+        
+        for(auto i:forward)
+            cout<<i<<" ";
+        cout<<endl;
+        backward[rat.size()-1]=1;
+        for(int i=rat.size()-2;i>=0;i--){
+            if(rat[i]>rat[i+1]){
+             backward[i]=backward[i+1]+1;
+         }else{
+             backward[i]=1;
+         } 
+        }
+        
+        for(auto i:backward)
+            cout<<i<<" ";
+        cout<<endl;
+        long result=0;
+        
+        for(int i=0;i<rat.size();i++){
+            cout<<max(forward[i],backward[i])<<" ";
+            result+=max(forward[i],backward[i]);
+        }
+        return result;
+    }
+};
